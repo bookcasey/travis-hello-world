433bbe2c45800b3e797173e51d3ca765f9adc142
Casey Olson <casey@bookcasey.com>
2016-05-03 21:54:13 -0500

    rename and nest describes

--- test/index-test.js
+++ test/index-test.js
@@ -8,17 +8,19 @@ var should = chai.should();
 
 chai.use(chaiHttp);
 
-describe('/hello', function() {
-    it('should return hello world', function() {
-        return chai.request(app)
-            .get('/hello')
-            .then(function(res, err) {
-              res.should.have.status(200);
-                  res.type.should.equal('application/json');
-                  res.charset.should.equal('utf-8');
-                  res.body.should.be.an('object');
-                  res.body.should.have.property('message');
-                  res.body.message.should.equal('Hello world!');
-            });
+describe('GET', function() {
+    describe('/hello', function() {
+        it('should return hello world', function() {
+            return chai.request(app)
+                .get('/hello')
+                .then(function(res, err) {
+                    res.should.have.status(200);
+                    res.type.should.equal('application/json');
+                    res.charset.should.equal('utf-8');
+                    res.body.should.be.an('object');
+                    res.body.should.have.property('message');
+                    res.body.message.should.equal('Hello world!');
+                });
+        });
     });
 });
