4e8582307288da41c2b86fb35541744387116991
Casey Olson <casey@bookcasey.com>
2016-05-03 22:27:21 -0500

    add casey route

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ app.get('/hello', function(req, res) {
     });
 });
 
+app.get('/casey', function(req, res) {
+  res.json({
+        message: 'Hello Casey!'
+    });
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
--- test/index-test.js
+++ test/index-test.js
@@ -23,4 +23,18 @@ describe('GET', function() {
                 });
         });
     });
+    describe('/casey', function() {
+        it('should return hello casey', function() {
+            return chai.request(app)
+                .get('/casey')
+                .then(function(res, err) {
+                    res.should.have.status(200);
+                    res.type.should.equal('application/json');
+                    res.charset.should.equal('utf-8');
+                    res.body.should.be.an('object');
+                    res.body.should.have.property('message');
+                    res.body.message.should.equal('Hello Casey!');
+                });
+        });
+    });
 });
